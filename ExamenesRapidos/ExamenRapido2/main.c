a055a5e7b122484bd93488c8126cbd1adbcc1b73
colina118 <mikehill2010@gmail.com>
2016-01-28 10:46:21 -0600

    examenRapido

--- ExamenesRapidos/ExamenRapido2/main.c
+++ ExamenesRapidos/ExamenRapido2/main.c
@@ -2,13 +2,18 @@
 #include <stdlib.h>
 
 typedef void (*t_iterate)(void * size);
-typedef void (*t_position)(void *);
+typedef int (*t_position)(void * );
+typedef void (*t_siguiente)();
 //typedef void (*t_rIterator)(void * size);
 
 void iterate(t_iterate, void *);
-void forward(void * vector, t_position begin, t_position end, t_position next);
+void forward(void * vector, t_position begin, t_position end, t_position next, void * final);
 void reverse(void* vector, t_position begin, t_position end, t_position prev);
 void bidirectional(void* vector, t_position begin, t_position end, t_position next, t_position prev);
+
+void intBegin(void*, void* );
+void intEnd(void* );
+void intNext(void* );
 
 int main(int argc, char **argv)
 {
@@ -16,9 +21,44 @@ int main(int argc, char **argv)
 	return 0;
 }
 
-void forward(void * vector, t_position begin, t_position end, t_position next);
+void forward(void * vector, t_position begin, t_position end, t_position next, void * final)
 {
-	void * principio = begin;
-	void * fin = end;
+	void * principio = (*begin)(vector);
+	void * fin = (*end)(final);
+	void * sig = next;
+	while(principio != fin)
+	{
+		printf(principio);
+		principio += sig;
+	}
 	
+}
+
+void reverse(void* vector, t_position begin, t_position end, t_position prev)
+{
+	void * principio = (*begin)(vector);
+	void * fin = (*end)(final);
+	void * ant = prev;
+	while(fin != principio)
+	{
+		printf(fin);
+		principio -= ant;
+	}
+}
+
+int intBegin(void * pa)
+{
+	int * a = (int *)pa;
+	return a;
+}
+
+int intEnd(void * pa)
+{
+	int * a = (int *)pa;
+	return a;
+}
+
+int intNext(void * pa)
+{
+	return sizeof(int);
 }
